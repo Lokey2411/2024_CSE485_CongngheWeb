d6899d015b050fed5192055fb502e65853275dc6
stevestea <130388059+stevestea@users.noreply.github.com>
2024-02-19 11:59:23 +0700

    style updated

--- project12/index.php
+++ project12/index.php
@@ -6,46 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <style>
-        * {
-            margin: 0;
-            padding: 0;
-            box-sizing: border-box;
-        }
-
-        body {
-            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-        }
-
-        nav {
-            background: #f1f1f1;
-            padding: 10px;
-        }
-
-        ul {
-            list-style: none;
-            margin: 0;
-            padding: 0;
-            display: flex;
-        }
-
-        li {
-            border-left: 1px solid red;
-            padding: 0 10px;
-            margin-right: 10px;
-        }
-
-        .container {
-            display: flex;
-            background-color: #f1f1f1;
-            align-items: center;
-        }
-
-        .house {
-            font-size: 25px;
-            margin: 0 10px
-        }
-    </style>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
--- project13/index.php
+++ project13/index.php
@@ -7,44 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Danh sách khóa học</title>
-    <style>
-        /* Thêm các định kiểu CSS của bạn ở đây */
-        .container {
-            display: flex;
-            flex-wrap: wrap;
-
-        }
-
-        .course {
-            background-color: #f9f9f9;
-            padding: 10px;
-            margin-bottom: 20px;
-            width: 30%;
-        }
-
-        .course h2 {
-            color: #333;
-        }
-
-        .course p {
-            color: #666;
-        }
-
-        .course-img {
-            width: 100%;
-            height: 200px;
-            object-fit: cover;
-        }
-
-        .gift {
-            font-size: 20px;
-        }
-
-        .course-info {
-            display: flex;
-            align-items: center;
-        }
-    </style>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
