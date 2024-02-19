37e35991b263c548f857354be85f38a48581a022
lokey2411 <hahaiviet24112003@gmail.com>
2024-02-19 16:46:57 +0700

    update prj12

--- project12/index.php
+++ project12/index.php
@@ -2,6 +2,7 @@
 <html lang="en">
 
 <head>
+    <script src="https://kit.fontawesome.com/1ce18db8ca.js" crossorigin="anonymous"></script>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
@@ -12,41 +13,99 @@
         box-sizing: border-box;
     }
 
+    nav {
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+    }
+
     nav>ul {
         display: flex;
         align-items: center;
         background-color: darkblue;
-        padding: 8px;
+        padding: 0 8px;
     }
 
     li {
         list-style: none;
+        color: #fff;
+        cursor: pointer;
+    }
+
+    .item {
         border-left: 1px solid #fff;
         text-transform: uppercase;
-        padding: 0 8px;
+        position: relative;
+        padding: 8px;
+    }
+
+    .subitems {
+        position: absolute;
+        top: 100%;
+        left: 0;
+        width: fit-content;
+        display: none;
+    }
+
+    .subitems>.sub-item {
+        background-color: cadetblue;
+        padding: 4px 8px;
         color: #fff;
+        border-top: 1px solid #eee;
+        /* display: none; */
+    }
+
+    .item:hover {
+        background-color: cyan;
+    }
+
+    .item:hover>.subitems,
+    .subitems:hover,
+    .sub-item:hover {
+        /* visibility: true;
+         */
+        display: block;
+        transition: 0.4s;
+    }
+
+    .icon {
+        border: none
     }
     </style>
 </head>
 
 <body>
     <?php
+    $eduItems = [
+    "Tuyển sinh đại học",
+    "Tuyển sinh thạc sĩ",
+    "Tuyển sinh tiến sĩ"
+    ];
 $navItems = [
- "GIỚI THIỆU",
- "TIN TỨC & THÔNG BÁO",
- "TUYỂN SINH",
- "ĐÀO TẠO",
- "NGHIÊN CỨU",
- "ĐỐI NGOẠI",
- "VĂN BẢN",
- "SINH VIÊN",
- "LIÊN HỆ"
+    ["title"=> "GIỚI THIỆU","subitems"=>[] ],
+    ["title"=> "TIN TỨC & THÔNG BÁO","subitems"=>[]],
+    ["title"=> "TUYỂN SINH","subitems"=>$eduItems],
+    ["title"=> "ĐÀO TẠO","subitems"=>[]],
+    ["title"=> "NGHIÊN CỨU","subitems"=>[]],
+    ["title"=> "ĐỐI NGOẠI","subitems"=>[]],
+    ["title"=> "VĂN BẢN","subitems"=>[]],
+    ["title"=> "SINH VIÊN","subitems"=>[]],
+    ["title"=> "LIÊN HỆ","subitems"=>[]]
 ];
-echo '<nav><ul>';
-foreach ($navItems as $item) {
- echo "<li>$item</li>";
-}
-echo '</ul></nav>';
+
+    echo '<nav><ul>';
+    echo "<li class='item icon'>
+    <i class='fa-solid fa-house'></i>
+    </li>";
+    foreach ($navItems as $item) {
+    echo "<li class='item'>{$item['title']} <ul class='subitems'>";
+        foreach ($item['subitems'] as $subitem) {
+        echo "<li class='sub-item'>$subitem</li>";
+        }
+    echo "</ul> </li>";
+    }
+    echo '</ul></nav>';
 ?>
 
 </body>
